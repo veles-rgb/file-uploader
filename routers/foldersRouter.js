caeff18ddfa17ac338666521b49e9e7c650d0ac0
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-05 13:22:38 -0500

    Add rename and delete folder routes

--- routers/foldersRouter.js
+++ routers/foldersRouter.js
@@ -4,6 +4,8 @@ const router = Router();
 const controller = require('../controllers/foldersController');
 
 router.post('/create', controller.postCreateFolder);
+router.post("/:id/rename", controller.renameFolder);
+router.post("/:id/delete", controller.deleteFolder);
 router.get('/:id', controller.getFolderById);
 
 
