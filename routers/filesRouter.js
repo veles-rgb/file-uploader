5c759930b64190f753f4265ccaedc2b9117e4cda
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-05 14:10:16 -0500

    Add /:id/delete route to delete files

--- routers/filesRouter.js
+++ routers/filesRouter.js
@@ -7,6 +7,7 @@ const upload = require("../middleware/upload");
 router.get('/', controller.renderFiles);
 router.get('/upload', controller.renderUploadForm);
 router.post('/upload', upload.single('file'), controller.postUploadFile);
+router.post('/:id/delete', controller.deleteFile);
 router.get('/:id', controller.renderFileById);
 
 module.exports = router;
