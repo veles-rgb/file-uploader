04a1527cc41f940dbb314b847caf4abd7eb3eee4
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-12 16:06:23 -0500

    Create /:id/unshare route for unsharing folders

--- routers/shareRouter.js
+++ routers/shareRouter.js
@@ -5,10 +5,13 @@ const controller = require("../controllers/shareController");
 
 router.get("/folder/:folderId", controller.renderShareForm);
 router.post("/folder/:folderId", controller.postShareToken);
-router.get('/:token', controller.renderShareView);
-// Navigate to a subfolder within shared folder
+
 router.get("/:token/folders/:folderId", controller.renderShareFolder);
-// View a file within shared folder
 router.get("/:token/files/:fileId", controller.renderShareFile);
+
+router.get("/:token", controller.renderShareView);
+
+router.post("/:id/unshare", controller.deleteShare);
+
 
 module.exports = router;
