a0abc5149e06214d09af4e0d6e29773e033b8e0e
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-01-19 12:32:03 -0500

    Remove blank space

--- app.js
+++ app.js
@@ -117,8 +117,6 @@ const assetsPath = path.join(__dirname, "public");
         });
     });
 
-
-
     const PORT = process.env.PORT || 3001;
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
